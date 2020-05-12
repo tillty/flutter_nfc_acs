e643fcfb70f8cc7f2368a5d8aa6fa8e498076fef
Lars Støttrup Nielsen <890725+larssn@users.noreply.github.com>
2020-05-12 13:04:02 +0200

    chore: simplified hex conversion

--- android/src/main/java/com/nuvopoint/flutter_nfc_acs/FlutterNfcAcsPlugin.java
+++ android/src/main/java/com/nuvopoint/flutter_nfc_acs/FlutterNfcAcsPlugin.java
@@ -304,7 +304,7 @@ public class FlutterNfcAcsPlugin extends BluetoothPermissions implements Flutter
       if (errorCode == BluetoothReader.ERROR_SUCCESS) {
         Log.i(TAG, "Authentication successful");
         Log.i(TAG, "Transmitting request to turn off ACR1255U-J1 sleep mode");
-        reader2.transmitEscapeCommand(Utils.hexString2Bytes(requestTurnOffSleepMode));
+        reader2.transmitEscapeCommand(Utils.hexStringToByteArray(requestTurnOffSleepMode));
         // When a compatible reader is detected, we hook up the event streams.
         cardStreamHandler.setReader(reader2);
         cardStreamHandler.startPolling();
--- android/src/main/java/com/nuvopoint/flutter_nfc_acs/Utils.java
+++ android/src/main/java/com/nuvopoint/flutter_nfc_acs/Utils.java
@@ -15,93 +15,104 @@ import android.text.Editable;
 import android.widget.EditText;
 
 class Utils {
-  public static String toHexString(byte[] array) {
+  static String toHexString(byte[] array) {
 
-    String bufferString = "";
+    StringBuilder bufferString = new StringBuilder();
 
     if (array != null) {
-      for (int i = 0; i < array.length; i++) {
-        String hexChar = Integer.toHexString(array[i] & 0xFF);
+      for (byte b : array) {
+        String hexChar = Integer.toHexString(b & 0xFF);
         if (hexChar.length() == 1) {
           hexChar = "0" + hexChar;
         }
-        bufferString += hexChar.toUpperCase(Locale.US) + " ";
+        bufferString.append(hexChar.toUpperCase(Locale.US)).append(" ");
       }
     }
-    return bufferString;
+
+    return bufferString.toString();
   }
 
-  private static boolean isHexNumber(byte value) {
-    if (!(value >= '0' && value <= '9') && !(value >= 'A' && value <= 'F')
-        && !(value >= 'a' && value <= 'f')) {
-      return false;
-    }
-    return true;
-  }
+//  private static boolean isHexNumber(byte value) {
+//    if (!(value >= '0' && value <= '9') && !(value >= 'A' && value <= 'F')
+//        && !(value >= 'a' && value <= 'f')) {
+//      return false;
+//    }
+//    return true;
+//  }
 
-  public static boolean isHexNumber(String string) {
-    if (string == null)
-      throw new NullPointerException("string was null");
+//  public static boolean isHexNumber(String string) {
+//    if (string == null)
+//      throw new NullPointerException("string was null");
+//
+//    boolean flag = true;
+//
+//    for (int i = 0; i < string.length(); i++) {
+//      char cc = string.charAt(i);
+//      if (!isHexNumber((byte) cc)) {
+//        flag = false;
+//        break;
+//      }
+//    }
+//    return flag;
+//  }
 
-    boolean flag = true;
+//  private static byte uniteBytes(byte src0, byte src1) {
+//    byte _b0 = Byte.decode("0x" + new String(new byte[] { src0 }))
+//        .byteValue();
+//    _b0 = (byte) (_b0 << 4);
+//    byte _b1 = Byte.decode("0x" + new String(new byte[] { src1 }))
+//        .byteValue();
+//    byte ret = (byte) (_b0 ^ _b1);
+//    return ret;
+//  }
 
-    for (int i = 0; i < string.length(); i++) {
-      char cc = string.charAt(i);
-      if (!isHexNumber((byte) cc)) {
-        flag = false;
-        break;
-      }
-    }
-    return flag;
-  }
-
-  private static byte uniteBytes(byte src0, byte src1) {
-    byte _b0 = Byte.decode("0x" + new String(new byte[] { src0 }))
-        .byteValue();
-    _b0 = (byte) (_b0 << 4);
-    byte _b1 = Byte.decode("0x" + new String(new byte[] { src1 }))
-        .byteValue();
-    byte ret = (byte) (_b0 ^ _b1);
-    return ret;
-  }
-
-  public static byte[] hexString2Bytes(String string) {
-    if (string == null)
-      throw new NullPointerException("string was null");
-
-    int len = string.length();
-
-    if (len == 0)
-      return new byte[0];
-    if (len % 2 == 1)
-      throw new IllegalArgumentException(
-          "string length should be an even number");
-
-    byte[] ret = new byte[len / 2];
-    byte[] tmp = string.getBytes();
-
+  static byte[] hexStringToByteArray(String s) {
+    int len = s.length();
+    byte[] data = new byte[len / 2];
     for (int i = 0; i < len; i += 2) {
-      if (!isHexNumber(tmp[i]) || !isHexNumber(tmp[i + 1])) {
-        throw new NumberFormatException(
-            "string contained invalid value");
-      }
-      ret[i / 2] = uniteBytes(tmp[i], tmp[i + 1]);
+      data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+          + Character.digit(s.charAt(i + 1), 16));
     }
-    return ret;
+    return data;
   }
 
-  public static byte[] getHexBytes2(String rawdata) {
-    if (rawdata == null || rawdata.isEmpty()) {
-      return null;
-    }
+//  static byte[] hexString2Bytes(String string) {
+//    if (string == null)
+//      throw new NullPointerException("string was null");
+//
+//    int len = string.length();
+//
+//    if (len == 0)
+//      return new byte[0];
+//    if (len % 2 == 1)
+//      throw new IllegalArgumentException(
+//          "string length should be an even number");
+//
+//    byte[] ret = new byte[len / 2];
+//    byte[] tmp = string.getBytes();
+//
+//    for (int i = 0; i < len; i += 2) {
+//      if (!isHexNumber(tmp[i]) || !isHexNumber(tmp[i + 1])) {
+//        throw new NumberFormatException(
+//            "string contained invalid value");
+//      }
+//      ret[i / 2] = uniteBytes(tmp[i], tmp[i + 1]);
+//    }
+//    return ret;
+//  }
 
-    String command = rawdata.replace(" ", "").replace("\n", "");
-
-    if (command.isEmpty() || command.length() % 2 != 0
-        || isHexNumber(command) == false) {
-      return null;
-    }
-
-    return hexString2Bytes(command);
-  }
+//  public static byte[] getHexBytes2(String rawdata) {
+//    if (rawdata == null || rawdata.isEmpty()) {
+//      return null;
+//    }
+//
+//    String command = rawdata.replace(" ", "").replace("\n", "");
+//
+//    if (command.isEmpty() || command.length() % 2 != 0
+//        || isHexNumber(command) == false) {
+//      return null;
+//    }
+//
+//    return hexString2Bytes(command);
+//  }
 }
